1b366b94c84e96a9efa451f641085462aed842be
darkprize <darkprize@naver.com>
2019-02-26 15:20:10 +0900

    1

--- app/src/main/java/andcom/nvchart/MainActivity.java
+++ app/src/main/java/andcom/nvchart/MainActivity.java
@@ -106,6 +106,8 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //test
+
         context = this;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
